d16878b42c9b57ddbd915bed2084b7f5c85b71b5
AnthonyEC7 <acardenast@est.ups.edu.ec>
2019-01-23 15:09:34 -0500

    CAMBIO DE 2 INTERFACES A 1 SOLA INTERFAZ

--- vista/interfaz1.py
+++ vista/interfaz1.py
@@ -2,11 +2,6 @@
 
 
 from tkinter import *
-root = Tk()
-root.title("RESERVA DE PARTIDOS ECUAVOLEY")
-ecuavoley = StringVar()
-label1 = Label(root, text="CANCHA LA LOMA").grid(row=0, column=0)
-
 
 
 
@@ -15,8 +10,19 @@ def verPartidos():
     textfield.insert(INSERT,infile.readlines())
     infile.close()
 
+def agregarPartidos():
 
-search_button = Button(root, text="Ver Partidos", command=verPartidos).grid(row=2, column=0)
+    agregar = Toplevel(root)
+    root.iconify()
+
+
+root = Tk()
+root.title("RESERVA DE PARTIDOS ECUAVOLEY")
+ecuavoley = StringVar()
+label1 = Label(root, text="CANCHA LA LOMA").grid(row=0, column=0)
+
+btnverPartidos = Button(root, text="Ver Partidos", command=verPartidos).grid(row=2, column=0)
+btnagregarPartidos = Button(root, text="Agregar Partido", command=agregarPartidos).grid(row=1, column=0)
 textfield = Text(root)
 textfield.grid(row=1, column=1, rowspan=2)
 root.mainloop()
